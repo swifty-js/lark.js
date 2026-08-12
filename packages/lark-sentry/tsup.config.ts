953aeabb1f3f961ced894f2817af9d10ac725abd
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-12 09:45:48 +0800

    feat: Introduce new features

--- packages/lark-sentry/tsup.config.ts
+++ packages/lark-sentry/tsup.config.ts
@@ -25,7 +25,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/plugins.ts", "src/vite.ts", "src/webpack.ts"],
   clean: true,
-  dts: { resolve: true },
+  dts: true,
   format: ["esm", "cjs"],
   minify: false,
   sourcemap: false,

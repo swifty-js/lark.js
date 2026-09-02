003963d527a2b59d8ecef25a6473d61a5ae8307c
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-09-02 14:06:07 +0800

    feat: Introduce new features

--- packages/charts/chartjs-lit-mvc/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import "@/components/wc-header";
-import "@/components/wc-footer";
-import "@/components/wc-auth-modal";
-import "@/components/wc-plaza-page";
-import "@/components/wc-projects-page";
-import "@/components/wc-help-page";
-import "@/components/wc-not-found-page";
-import "@/components/wc-editor-page";
